49032dc9e6527b3093f9cce741360044db1336c7
mjolk <2mjolk@gmail.com>
2026-01-27 12:36:01 +0100

    no c-sharp, use neocmakelsp, add black python formatter, remove nvchad terminal escape mapping <C-x>

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,6 +15,8 @@ vim.keymap.set("n", "<leader>k", function()
     end,
   })
 end)
+vim.keymap.del("t", "<C-x>")
+--map("t", "<C-x>", "<C-w>s", { desc = "remap ctrl-x to open in horizontal split" })
 -- DAP
 map("n", "<leader>dB", function()
   require("dap").set_breakpoint(vim.fn.input "Breakpoint condition: ")
